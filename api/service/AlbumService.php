6751310b6b7021d1ebfe2401307d628e8940a109
Troy Mass <masstroy21@gmail.com>
2015-08-01 15:52:48 -0500

    Created album listing API

--- api/service/AlbumService.php
+++ api/service/AlbumService.php
@@ -4,12 +4,15 @@ require_once('dto/Album.php');
 class AlbumService {
 
 	public function listAlbums($albumRootDirectory) {
+		$files = scandir($albumRootDirectory, SCANDIR_SORT_ASCENDING);
+		
 		$albumList = array();
-	
-		for ($i = 1; $i <= 7; $i++) {
-			$name = "PhotoAlbum" . $i;
-			$album = new Album($name, "some/path", "2015-07-30");
-			array_push($albumList, $album);
+		foreach ($files as $file) {
+			$path = rtrim($albumRootDirectory, '/') . "/" . $file;
+			if (is_dir($path) && $file != "." && $file != "..") {
+				$album = new Album($file, $path, null);
+				array_push($albumList, $album);
+			}
 		}
 	
 		return $albumList;
